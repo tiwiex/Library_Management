c545e1ef0e8abe6330a4adf215115fcedcf93512
Taiwo Akinosho <tiwiex@yahoo.com>
2023-05-10 22:28:45 +0000

    still adjusting route

--- library_management/library_management/doctype/library_article/library_article.py
+++ library_management/library_management/doctype/library_article/library_article.py
@@ -2,8 +2,8 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
-class LibraryArticle(Document):
+class LibraryArticle(WebsiteGenerator):
 	def before_save(self):
 		self.route = f'{self.article_name}'
