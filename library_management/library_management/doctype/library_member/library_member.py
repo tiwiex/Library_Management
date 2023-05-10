f48c4e62d4681d6b5d905341c4f04817efd6c64d
Taiwo Akinosho <tiwiex@yahoo.com>
2023-05-10 12:25:14 +0100

    didn't work. still checking

--- library_management/library_management/doctype/library_member/library_member.py
+++ library_management/library_management/doctype/library_member/library_member.py
@@ -6,4 +6,4 @@ from frappe.model.document import Document
 
 class LibraryMember(Document):
 	def before_save(self):
-        self.full_name = f'{self.first_name} {self.last_name or ""}'
+    	self.full_name = f'{self.first_name} {self.last_name or ""}'
