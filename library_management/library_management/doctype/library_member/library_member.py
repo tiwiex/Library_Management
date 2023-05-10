348b82bd0eceab130bbe66e69b4a67cfecd01af3
Taiwo Akinosho <tiwiex@yahoo.com>
2023-05-10 12:28:04 +0100

    sorted tab space

--- library_management/library_management/doctype/library_member/library_member.py
+++ library_management/library_management/doctype/library_member/library_member.py
@@ -6,5 +6,4 @@ from frappe.model.document import Document
 
 class LibraryMember(Document):
 	def before_save(self):
-	#comment test
-    	self.full_name = f'{self.first_name} {self.last_name or ""}'
+		self.full_name = f'{self.first_name} {self.last_name or ""}'
